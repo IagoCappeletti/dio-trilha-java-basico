a4b12e2d83075447ed1590a41c1817d75e3185a2
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-23 14:16:58 -0300

    Sistema de Conta Bancário

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("Por favor, digite o número da Conta: ");
+            int numero = sc.nextInt();
+
+            System.out.print("Por favor, digite o número da Agência: ");
+            String agencia = sc.next();
+
+            System.out.print("Digite o seu Nome: ");
+            sc.nextLine();
+            String nomeCompleto = sc.nextLine();
+
+            System.out.print("Digite o valor do Saldo: ");
+            Double saldo = sc.nextDouble();
+
+            System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        sc.close();
     }
 }
