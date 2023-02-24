6e4341f6ec23e905310dae1a50fe7538ed47fd32
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-24 00:03:33 -0300

    laços-forEach-com-arrays

--- controle-de-fluxo/src/ExemploForArray.java
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -3,9 +3,15 @@ public class ExemploForArray {
         
         String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int x = 0; x < alunos.length; x++) {
-            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
-        }
+        /*
+            for (int x = 0; x < alunos.length; x++) {
+                System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+            }
+        */
+
+            for(String aluno : alunos){
+                System.out.println("Nome do aluno é: " + aluno);
+            }
 
     }
 }
