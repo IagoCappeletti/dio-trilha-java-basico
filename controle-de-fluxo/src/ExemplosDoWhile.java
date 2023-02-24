24cdd4799cb16910b77653755331f1445ef61378
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-24 01:20:20 -0300

    adicionei comentários

--- controle-de-fluxo/src/ExemplosDoWhile.java
+++ controle-de-fluxo/src/ExemplosDoWhile.java
@@ -4,18 +4,19 @@ public class ExemplosDoWhile {
         
         System.out.println("Discando...");
         do {
-            //executando repetidas vezes até alguém atender
+            // Executa repetidamente até alguém atender
             System.out.println("Telefone tocando");
-        } while(tocando());
+        } while (tocando());
 
         System.out.println("Alô ");
 
     }
 
-    private static boolean tocando (){
+    private static boolean tocando () {
+         // Determina se alguém atendeu ou não
         boolean atendeu = new Random().nextInt(3)==1;
         System.out.println("Atendeu? " + atendeu);
-        //negando o ato de continuar tocando
+        // Retorna true se ninguém atendeu (ainda tocando)
         return ! atendeu;
     }
 }
