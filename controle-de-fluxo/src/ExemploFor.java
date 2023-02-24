ba3c581124d4166ea4642b18ac77b1f4e90440d9
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-23 23:40:07 -0300

    laços-for

--- controle-de-fluxo/src/ExemploFor.java
+++ controle-de-fluxo/src/ExemploFor.java
@@ -4,6 +4,7 @@ public class ExemploFor {
         for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
             System.out.println("Contando carneirinhos: " + carneirinhos);
         }
-        
+
+        System.out.println("\nJoãozinho caiu no sono !!!");
     }
 }
